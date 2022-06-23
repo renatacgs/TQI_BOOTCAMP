1e1b48ee11f1769e552a09dd3de62668393787cf
Renata Cristina <renatinha.8@hotmail.com>
2022-06-23 19:27:03 -0300

    SPRING

---
+++ PADROES_PROJETO/src/main/java/one/digitalinnovation/gof/TqiBootcampPadroesProjetoSpringApplication.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ *
+ * @author brenodveronezi
+ *
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class TqiBootcampPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqiBootcampPadroesProjetoSpringApplication.class, args);
+	}
+
+}
